1ea96f4934265aed708a3e5889b7512804927565
Kornil17 <Dimitra4@mail.ru>
2026-06-27 21:57:25 +0300

    TASK-6 Added all tasks routes plugs.

--- di/api_routes.py
+++ di/api_routes.py
@@ -10,7 +10,7 @@ from dependency_injector.providers import (
 )
 from fastapi import APIRouter
 
-from src.presentation.dtos.models import Error, Task
+from src.presentation.dtos.models import Comment, Error, Task
 from src.presentation.routers.tasks import TasksRouter
 
 
@@ -29,10 +29,88 @@ class ApiRoutesContainer(DeclarativeContainer):
         _routes_config_data=List(
             Dict(
                 path="/{task_id}",
-                endpoint="get_task_by_id",
+                endpoint="get",
                 methods=[HTTPMethod.GET],
                 responses={
                     HTTPStatus.OK: {"model": Task},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}",
+                endpoint="delete",
+                methods=[HTTPMethod.DELETE],
+                responses={
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}",
+                endpoint="edit",
+                methods=[HTTPMethod.PATCH],
+                responses={
+                    HTTPStatus.OK: {"model": Task},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="",
+                endpoint="create",
+                methods=[HTTPMethod.POST],
+                responses={
+                    HTTPStatus.CREATED: {"model": Task},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}/executor",
+                endpoint="assign_executor",
+                methods=[HTTPMethod.POST],
+                responses={
+                    HTTPStatus.OK: {"model": Task},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}/status",
+                endpoint="update_status",
+                methods=[HTTPMethod.PATCH],
+                responses={
+                    HTTPStatus.OK: {"model": Task},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.BAD_REQUEST: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}/deadline",
+                endpoint="update_deadline",
+                methods=[HTTPMethod.PATCH],
+                responses={
+                    HTTPStatus.OK: {"model": Task},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.BAD_REQUEST: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}/comments",
+                endpoint="add_comments",
+                methods=[HTTPMethod.POST],
+                responses={
+                    HTTPStatus.CREATED: {"model": Comment},
+                    HTTPStatus.NOT_FOUND: {"model": Error},
+                    HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
+                },
+            ),
+            Dict(
+                path="/{task_id}/comments",
+                endpoint="get_comments",
+                methods=[HTTPMethod.GET],
+                responses={
+                    HTTPStatus.OK: {"model": Comment},
                     HTTPStatus.NOT_FOUND: {"model": Error},
                     HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Error},
                 },
--- src/presentation/routers/tasks.py
+++ src/presentation/routers/tasks.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass
-from typing import final
+from typing import Annotated, final
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
-from src.presentation.dtos.models import Error, Task
+from src.presentation.dtos.models import (
+    AppointmentExecutorTaskData,
+    Comment,
+    CommentsTaskData,
+    CreateTaskData,
+    EditTaskData,
+    Error,
+    Task,
+    UpdateDeadlineData,
+    UpdateStatusData,
+)
 from src.presentation.routers import RoterConfigData
 
 
@@ -25,11 +35,59 @@ class TasksRouter:
                 },
             )
 
-    async def get_task_by_id(self, task_id: int) -> Task | Error:  # type: ignore[empty-body]
+    async def get(self, task_id: int) -> Task | Error:  # type: ignore[empty-body]
         """Получение задачи по ID."""
 
-    async def delete_task_by_id(self, task_id: int) -> Task | Error:  # type: ignore[empty-body]
+    async def delete(self, task_id: int) -> Error:  # type: ignore[empty-body]
         """Удаление задачи по ID."""
+
+    async def edit(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        task_data: Annotated[EditTaskData, Body()],
+    ) -> Task | Error:
+        """Редактирование данных по задаче по ID."""
+
+    async def create(  # type: ignore[empty-body]
+        self,
+        task_data: Annotated[CreateTaskData, Body()],
+    ) -> Task | Error:
+        """Создание новой задачи."""
+
+    async def assign_executor(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        task_data: Annotated[AppointmentExecutorTaskData, Body()],
+    ) -> Task | Error:
+        """Назначение исполнителя задачи."""
+
+    async def update_status(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        status_data: Annotated[UpdateStatusData, Body()],
+    ) -> Task | Error:
+        """Обновление статуса задачи."""
+
+    async def update_deadline(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        deadline_data: Annotated[UpdateDeadlineData, Body()],
+    ) -> Task | Error:
+        """Обновление срока выполнения задачи."""
+
+    async def add_comments(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        comment_data: Annotated[CommentsTaskData, Body()],
+    ) -> Comment | Error:
+        """Добавление комментариев в задачу."""
+
+    async def get_comments(  # type: ignore[empty-body]
+        self,
+        task_id: int,
+        comment_data: Annotated[CommentsTaskData, Body()],
+    ) -> Comment | Error:
+        """Получение комментариев из задачи."""
 
     @property
     def router(self) -> APIRouter:
